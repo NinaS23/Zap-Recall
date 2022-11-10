6657ac8a85f43b7f8ff23fd1105bba6e1e9ecf5c
MariaDuda <mariabevenuto062@gmail.com>
2022-11-10 14:40:40 -0300

    feat: input at entry screen

--- src/pages/Entry/index.js
+++ src/pages/Entry/index.js
@@ -1,5 +1,5 @@
 import logo from "../../assets/imgs/logo.png";
-import { Content } from "./style";
+import { Content, InputMeta} from "./style";
 
 export default function Entry() {
     return (
@@ -8,6 +8,8 @@ export default function Entry() {
                 <img src={logo} alt="logo" />
                 <h1>ZapRecall</h1>
             </div>
+            <InputMeta type="text" id="meta" name="input-meta" placeholder="Digite sua meta de zaps..."></InputMeta>
+            <button>Iniciar Recall!</button>
         </Content>
     );
 }
--- src/pages/Entry/style.js
+++ src/pages/Entry/style.js
@@ -3,33 +3,48 @@ import styled from "styled-components";
 const Content = styled.div`
     display: flex;
     align-items: center;
+    flex-direction: column;
     justify-content: center;
     div{
-      margin-top: 150px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      img{
-        width: 90px;
-      }
-      h1{
-        /* ZapRecall */
-         margin-top: 5px;
-          font-family: 'Righteous';
-          font-style: normal;
-          font-weight: 400;
-          font-size: 40px;
-          line-height: 31px;
-          display: flex;
-          align-items: center;
-          text-align: center;
-          letter-spacing: -0.012em;
-          color: #FFFFFF;
-
-      }
-    }
+        margin-top: 150px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        img{
+          width: 90px;
+         }
+         h1{
+            margin-top: 5px;
+            font-family: 'Righteous';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 40px;
+            line-height: 31px;
+            display: flex;
+            align-items: center;
+            text-align: center;
+            letter-spacing: -0.012em;
+            color: #FFFFFF;
+          }
+  }
 
 `
+const InputMeta = styled.input`
+  width: 200px;
+  height: 37px;
+  background: #FFFFFF;
+  margin-top: 60px;
+  border-radius: 11px;
+  ::placeholder{
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    letter-spacing: -0.012em; 
+    color: #808080;
+  }
+`
 export {
-  Content
+  Content,
+  InputMeta
 };
