8556db4c9c9a78e9aab46df8319b89d8dea187c0
MariaDuda <mariabevenuto062@gmail.com>
2022-11-15 12:38:35 -0300

    feat: user can  not try again if click at error , almost or correct img

--- src/components/ClosedCard/index.js
+++ src/components/ClosedCard/index.js
@@ -4,16 +4,16 @@ import correct from "../../assets/imgs/correct.svg";
 import almost from "../../assets/imgs/almost.svg";
 
 export default function CardClosed({ index, picked, tapCard, status }) {
-
+    
     function getIcon() {
         if (status === "not_answer") {
             return <ion-icon className="arrow" name="play-outline" onClick={() => tapCard(index)}></ion-icon>
         } else if (status === "correct") {
-            return < img src={correct} alt="correct" onClick={() => tapCard(index)}/>
+            return < img src={correct} alt="correct" />
         } else if (status === "error") {
-            return < img src={error} alt="error" onClick={() => tapCard(index)} />
+            return < img src={error} alt="error" />
         } else {
-            return < img src={almost} alt="almost" onClick={() => tapCard(index)} />
+            return < img src={almost} alt="almost" />
         }
     }
     return (
